6f3844ca3409738df0bfc977f3537746ce4e8a7d
pabloRedolar <predolars@iesch.org>
2024-10-09 21:21:05 +0200

    ud2 contenido

---
+++ UD2/primerRest/src/main/java/org/iesch/primerRest/PrimerRestApplication.java
@@ -0,0 +1,21 @@
+package org.iesch.primerRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+class PrimerRestAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerRestAplication.class, args);
+		hello("Pablo");
+    }
+
+    @GetMapping("/hello")
+    public static String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ UD2/primerRest/src/test/java/org/iesch/primerRest/PrimerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesch.primerRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
