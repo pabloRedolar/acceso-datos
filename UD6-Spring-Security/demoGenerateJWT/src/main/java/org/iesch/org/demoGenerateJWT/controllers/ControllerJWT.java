2b6185c37c362d8515fdb17ea3f09dd12d9c6efa
pabloRedolar <predolars@iesch.org>
2025-02-05 16:31:06 +0100

    ejers basicos ud06 jwt

--- UD6-Spring-Security/demoGenerateJWT/src/main/java/org/iesch/org/demoGenerateJWT/controllers/ControllerJWT.java
+++ UD6-Spring-Security/demoGenerateJWT/src/main/java/org/iesch/org/demoGenerateJWT/controllers/ControllerJWT.java
@@ -1,11 +1,14 @@
 package org.iesch.org.demoGenerateJWT.controllers;
 
+import io.jsonwebtoken.Claims;
 import org.iesch.org.demoGenerateJWT.utilJWT.JWTutil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ControllerJWT {
@@ -16,5 +19,15 @@ public class ControllerJWT {
     @GetMapping("/genera/{username}")
     public String generarJsonWebToken(@PathVariable String username) {
         return JWTutil.generateToken(username);
+    }
+
+    @GetMapping("/buscarClaims")
+    public Claims buscarClaims(@RequestParam String jwt) {
+        return JWTutil.extractAllClaims(jwt);
+    }
+
+    @GetMapping("/buscarClaims")
+    public Boolean urlValida(@RequestParam String jwt) {
+        return JWTutil.;
     }
 }
