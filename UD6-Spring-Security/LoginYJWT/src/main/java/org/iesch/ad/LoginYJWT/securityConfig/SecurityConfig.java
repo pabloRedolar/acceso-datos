2abc6ab0216bb93d54a250c4b20f257a1005005c
pabloRedolar <predolars@iesch.org>
2025-02-14 20:40:11 +0100

    a

--- UD6-Spring-Security/LoginYJWT/src/main/java/org/iesch/ad/LoginYJWT/securityConfig/SecurityConfig.java
+++ UD6-Spring-Security/LoginYJWT/src/main/java/org/iesch/ad/LoginYJWT/securityConfig/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
