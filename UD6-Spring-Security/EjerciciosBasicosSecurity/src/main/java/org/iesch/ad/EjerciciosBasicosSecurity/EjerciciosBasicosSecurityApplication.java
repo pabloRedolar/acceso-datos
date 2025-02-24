91679b28d3ac392ff32e9ac6974c305908bb772f
pabloRedolar <predolars@iesch.org>
2025-02-24 21:16:01 +0100

    ejers basicos seguridad spring

---
+++ UD6-Spring-Security/EjerciciosBasicosSecurity/src/main/java/org/iesch/ad/EjerciciosBasicosSecurity/EjerciciosBasicosSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.iesch.ad.EjerciciosBasicosSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosBasicosSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosBasicosSecurityApplication.class, args);
+	}
+
+}
---
+++ UD6-Spring-Security/EjerciciosBasicosSecurity/src/test/java/org/iesch/ad/EjerciciosBasicosSecurity/EjerciciosBasicosSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesch.ad.EjerciciosBasicosSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosBasicosSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
